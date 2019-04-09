0a8c1bee52d2e1273f8f490cf3b20c9067143a2b
poshepjl <32207101+poshepjl@users.noreply.github.com>
2019-04-09 15:52:20 -0500

    Main Menu
    Main Menu, Button Manager, Test Script

---
+++ CSG 119 Final/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,45 @@
+﻿//Jake Poshepny
+//4 9 19
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public List<GameObject> UIPanels = new List<GameObject>();
+    public GameObject startPanel;
+
+    private void Awake()
+    {
+        //Turn off all panels
+        foreach (GameObject p in UIPanels)
+        {
+            p.SetActive(false);
+        }
+
+        //Turn on the default starting panel
+        startPanel.SetActive(true);
+    }
+
+    //Load a Scene using a Button press
+    public void BM_LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    //Quit the Application
+    public void BM_Quit()
+    {
+        Application.Quit();
+    }
+
+    //Change the current UI Panel that is being viewed
+    public void BM_ChangeUIPanel(GameObject newPanel)
+    {
+        startPanel.SetActive(false);
+        startPanel = newPanel;
+        startPanel.SetActive(true);
+    }
+}
---
+++ CSG 119 Final/Assets/Scripts/TestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestScript : MonoBehaviour
+{
+    public bool flag;
+    public int i = 1;
+
+    public void Console()
+    {
+        Debug.Log("Does this even work???");
+    }
+}
+
+[CustomEditor(typeof(TestScript))]
+public class TestScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var testScript = target as TestScript;
+
+        testScript.flag = GUILayout.Toggle(testScript.flag, "Flag");
+
+        if(testScript.flag)
+        {
+            testScript.i = EditorGUILayout.IntSlider("I Field:", testScript.i, 1, 100);
+
+            if (GUILayout.Button("Button"))
+            {
+                testScript.Console();
+            }
+        }
+    }
+}
