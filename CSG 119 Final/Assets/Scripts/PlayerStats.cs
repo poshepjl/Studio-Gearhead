c679797e68db30a85192a69ff2f52d95631719c6
TorrelLloyd <36899107+TorrelLloyd@users.noreply.github.com>
2019-04-24 16:07:27 -0500

    THE BEST CHANGES YOLO
    RAWRRRRRRRRRRRRRR

--- CSG 119 Final/Assets/Scripts/PlayerStats.cs
+++ CSG 119 Final/Assets/Scripts/PlayerStats.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class PlayerStats : MonoBehaviour
 {
     public float maxHealth;
-    [HideInInspector] public float currentHealth;
+    public float currentHealth;
 
     public Slider healthSlider;
 
