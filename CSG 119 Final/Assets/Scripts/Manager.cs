b0c7904f7580c5d8d23e82e371b4575912e81213
Jake Poshepny <32207101+poshepjl@users.noreply.github.com>
2019-05-01 14:51:10 -0500

    things

---
+++ CSG 119 Final/Assets/Scripts/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+
+    public void GameOver()
+    {
+
+    }
+}
