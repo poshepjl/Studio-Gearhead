2052ae7012a317b6cfc3abea79b5c3c196f20448
TorrelLloyd <36899107+TorrelLloyd@users.noreply.github.com>
2019-05-08 15:25:07 -0500

    Sceen tutorial

---
+++ CSG 119 Final/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    public Image switchImage;
+    public Sprite[] tutorialSprites;
+    [SerializeField]private int current = 0;
+
+    public void NextImage(Object yolo)
+    {
+        if (current < 5)
+            current++;
+        else
+            SceneManager.LoadScene(yolo.name);
+
+
+        switchImage.sprite = tutorialSprites[current];
+
+
+
+
+    }
+    public void PreviousImage()
+    {
+        if (current > 0)
+            current--;
+
+        switchImage.sprite = tutorialSprites[current];
+    }
+}
