80d2ed2d404c2ceb6a0719c2aa8c0fd6ac47ebca
TorrelLloyd <36899107+TorrelLloyd@users.noreply.github.com>
2019-05-15 14:55:32 -0500

    Updated Player Controls

--- CSG 119 Final/Assets/Scripts/PlayerControls.cs
+++ CSG 119 Final/Assets/Scripts/PlayerControls.cs
@@ -6,7 +6,55 @@ using UnityEngine.UI;
 public class PlayerControls : MonoBehaviour
 {
     [Header("Player Components")]
-    public Slider valueMove;
+    public Vector3 endPoint;
 
-    public void ConvertMove() { transform.localEulerAngles = new Vector3(0, valueMove.value, 0); Debug.Log("Pro Strats"); }
+
+    private void Update()
+    {
+        #if UNITY_EDITOR
+        EditorMovement();
+        #endif
+
+        #if UNITY_ANDROID
+        AndroidMovement();
+        #endif
+    }
+    void EditorMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                endPoint = hit.point;
+                Vector3 lookPos = new Vector3(endPoint.x, 0, hit.point.z);
+                Debug.Log(endPoint);
+                transform.LookAt(lookPos);
+                transform.localEulerAngles += new Vector3(transform.rotation.x, transform.rotation.y + 70, transform.rotation.z);
+
+            }
+        }
+    }
+    void AndroidMovement()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            RaycastHit hit;
+            Ray ray;
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Physics.Raycast(ray, out hit))
+            {
+                endPoint = hit.point;
+                Vector3 lookPos = new Vector3(endPoint.x, 0, hit.point.z);
+                transform.LookAt(lookPos);
+                transform.localEulerAngles += new Vector3(transform.rotation.x, transform.rotation.y + 70, transform.rotation.z);
+
+            }
+
+        }
+    }
 }
