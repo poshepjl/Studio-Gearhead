737c72038bbc851e5e790d7c5fb21978e35cfb9e
Jake Poshepny <32207101+poshepjl@users.noreply.github.com>
2019-05-22 14:33:59 -0500

    New Art Scenes

---
+++ CSG 119 Final/Assets/Scripts/LevelLoad2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad2 : MonoBehaviour
+{
+    public void LoadLevel(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
